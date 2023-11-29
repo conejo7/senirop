8eb17fea904ece7ddb2c553b9d4bdc30b9d903bf
conejo7 <erick_s5490@hotmail.com>
2023-11-29 13:29:14 -0500

    fix NearPalindrome problem 2 y statementen add problem 3

--- problema2.js
+++ problema2.js
@@ -1,4 +1,3 @@
-
 /** A string is called a palindrome if it reads the same forwards and backwards. E.g., "a",
  *  "noon" and "tacocat" are palindromes but "cocoa" isn't.
  A string is called a near-palindrome if we can rearrange its characters to make it a palindrome. For example, "aaa",
@@ -11,16 +10,16 @@
  Determine and return the smallest number of operations needed to turn S into a near-palindrome.
 
  Definition
- Class:	NearPalindromesDiv1
- Method:	Solve
- Parameters:	String
- Returns:	Int
- Method signature:	int solve(String S)
+ Class:    NearPalindromesDiv1
+ Method:    Solve
+ Parameters:    String
+ Returns:    Int
+ Method signature:    int solve(String S)
  (be sure your method is public)
 
  Constraints
- -	S will contain between 1 and 2,500 characters, inclusive.
- -	Each character of S will be a lowercase English letter ('a'-'z').
+ -    S will contain between 1 and 2,500 characters, inclusive.
+ -    Each character of S will be a lowercase English letter ('a'-'z').
 
   
  Examples
@@ -51,35 +50,48 @@ function solve(cadenaP) {
     if (cadenaP.length < 1 || cadenaP.length > 2500) {
         throw new Error("La cadena S debe tener entre 1 y 2500 caracteres.");
     }
-    cadenaP=cadenaP.toLowerCase();
+    cadenaP = cadenaP.toLowerCase();
     const lon = cadenaP.length;
     let operations = 0;
-
-    for (let i = 0; i < (lon / 2); i++) {
-        const inicio = cadenaP[i];
-        const final = cadenaP[lon - 1 - i];
-        const diff = Math.abs(final.charCodeAt(0) - inicio.charCodeAt(0));
-        operations += Math.min(diff, 26 - diff);
-        console.log("operations:"+operations);
-    }
-    if (operations<3) {
+    const result = NearPalindrome(cadenaP);
+    console.log(result); // Devuelve true
+    if (result) {
         return "This is already a near-palindrome";
+    } else {
+        for (let i = 0; i < (lon / 2); i++) {
+            const inicio = cadenaP[i];
+            const final = cadenaP[lon - 1 - i];
+            const diff = Math.abs(final.charCodeAt(0) - inicio.charCodeAt(0));
+            operations += Math.min(diff, 26 - diff);
+            // console.log("operations:"+operations);
+        }
+        return operations;
     }
-
-    return operations;
-
 }
-//const cadena = "daddy";
-const cadenaPalindromo= "cocoasdfsdf";
+
 //ejemplo:  abcdefgh
 //de a hasta la h = 7
 //de b hasta la g = 5
 //de c hasta la f = 3
 //de d hasta la e = 1
 //suma  = 16
-console.log("cocoa ->"+solve("cocoa"));
-console.log("daddy ->"+solve("daddy"));
-console.log("abcdefgh ->"+solve("abcdefgh"));
+console.log("cocoa ->" + solve("cocoa"));
+console.log("daddy ->" + solve("daddy"));
+console.log("abcdefgh ->" + solve("abcdefgh"));
 
+function NearPalindrome(cadenaP) {
 
-
+    const contarChar = {};
+    for (const char of cadenaP) {
+        contarChar[char] = (contarChar[char] || 0) + 1;
+        console.log(contarChar)
+    }
+    //  near-palindrome tiene más de un carácter con frecuencia impar
+    let impar = 0;
+    for (const char in contarChar) {
+        if (contarChar[char] % 2 !== 0) {
+            impar++;
+        }
+    }
+    return impar <= 1;
+}
--- problema3.js
+++ problema3.js
@@ -1,5 +1,63 @@
-function makeDistinct(dials) {
-    // Utilizar un conjunto para eliminar duplicados
+
+/**
+You have a bicycle lock. The locking mechanism consists of several dials. Each dial contains the digits 0-9 in a cycle, in this order. From each dial exactly one digit is visible.
+    Each dial can be rotated in either direction:
+    •	Rotating the dial one step in the positive direction (denoted '+') increments the digit shown.
+
+    E.g., if the dial currently shows 4 and you make the '+' rotation, the dial will now show 5. After the digit 9 comes the digit 0 again.
+•	Rotating the dial one step in the negative direction (denoted '-') does the opposite.
+
+    E.g., the '-' rotation will change a dial showing 5 into a dial showing 4, and it will change a dial showing 0 into a dial showing 9.
+Your finger is currently on the leftmost dial. You can use it to rotate the dial it's on. You can also move your finger one dial to the right (denoted '>') or one dial to the left (denoted '<'). You can only move your finger if the destination dial actually exists.
+You are given the int[] dials. The elements of dials are the digits currently shown on the dials of your lock, from the left to the right.
+    You would like to scramble your lock into a state where all of the digits shown on the dials are mutually distinct. Find any sequence of at most 100 actions that accomplishes this goal, and return it as a String[].
+
+    Definition
+Class:	BicycleLock
+Method:	makeDistinct
+Parameters:	int[]
+Returns:	String
+Method signature:	String makeDistinct(int[] dials)
+(be sure your method is public)
+
+Notes
+-	You are not required to minimize the number of operations.
+
+    Constraints
+-	dials will contain between 1 and 10 elements, inclusive.
+-	Each element of dials will be between 0 and 9, inclusive.
+    Examples
+0)
+{3, 2, 7}
+Returns: ""
+All dials already show distinct numbers, so we do not have to do anything at all.
+
+1)
+{8, 8, 0, 9}
+
+Returns: ">+++"
+Our solution moves the finger to the second dial and then rotates it three times in the positive direction. This changes the lock from its initial state 8809 into 8909, then 8009, and finally 8109, which is a valid final state.
+    There are many other valid solutions. The most efficient one is to simply rotate the first dial once in the negative direction, changing the lock to 7809.
+
+
+2)
+{9}
+
+Returns: "+++--+-"
+Again, we do not have to do anything, but we can. Remember that any solution that consists of at most 100 actions will be accepted.
+
+
+3)
+{0, 1, 0, 2, 0}
+
+Returns: "
+
+    .+"
+The returned solution produces the dial values {0, 1, 5, 3, 9} in the end.
+*/
+
+    function makeDistinct(dials) {
+
     const digitSet = new Set(dials);
 
     if (digitSet.size === dials.length) {
@@ -9,42 +67,34 @@ function makeDistinct(dials) {
         let actions = "";
         for (let i = 0; i < dials.length; i++) {
             // console.log("dial:"+dials[i]);
+            if (dials[i] === dials[i+1]) {
+                actions += ">".repeat(1);
+            }
             for (let j =  1; j < dials.length; j++) {
                 if (i===j){
                     j++;
                 }
                 if (dials[i] === dials[j]) {
-                    // console.log("dial i:"+dials[i]);
-                    // console.log("dial j:"+dials[j]);
-                    // console.log(i);
                     aux[j] = (dials[j] + 1) % 10; // Aumentar en uno
-                }
-                actions += ">".repeat(i);
-                // console.log("acciones:" + actions);
-                // Realizar las rotaciones necesarias para llegar al próximo dígito único
-                const nextDigit = (dials[j] + 1) % 10;
-                if (nextDigit > dials[j]) {
-                    actions += "+".repeat(nextDigit - dials[j]);
-                } else if (nextDigit < dials[j]) {
-                    actions += "-".repeat(dials[j] - nextDigit);
+                    actions += "+";
                 }
             }
         }
-
+        console.log(aux);
         // return aux;
         return actions;
     }
 
 }
 
-const digits0 = [3,2,7];
-const digits1 = [1, 2, 3, 4, 5];
-const digits2 = [8,8,0,9];
-const digits3 = [0, 1, 0, 2,0];
-console.log("digits0"+makeDistinct(digits0));
-console.log("digits1"+makeDistinct(digits1));
-console.log("digits2"+makeDistinct(digits2));
-console.log("digits3"+makeDistinct(digits3));
+const ejemplo1 = [3,2,7];
+const ejemplo2 = [8,8,0,9];
+const ejemplo3 = [9]
+const ejemplo4 = [0, 1, 0, 2,0];
+console.log("ejemplo1:"+makeDistinct(ejemplo1));
+console.log("ejemplo2:"+makeDistinct(ejemplo2));
+console.log("ejemplo3:"+makeDistinct(ejemplo3));
+console.log("ejemplo4:"+makeDistinct(ejemplo4));
 
 
 
